1274ecbb331624eec2fe1af7a8f1999bcd024f1a
zxj23333 <2372912451zxj@gmail.com>
2020-06-28 10:17:36 +0800

    Docker安装脚本增加Docker-Compose安装支持

--- src/linux/install_docker.sh
+++ src/linux/install_docker.sh
@@ -3,25 +3,25 @@
 # 依赖列表
 DOCKER_DEPEND_LIST=("yum-utils" "device-mapper-persistent-data" "lvm2")
 
-SOFTWARE_NAME="docker-ce"
+DOCKER_NAME="docker-ce"
 
-read -rep "是否开始运行${SOFTWARE_NAME}安装脚本? (y/n): " flag
+read -rep "是否开始运行${DOCKER_NAME}安装脚本? (y/n): " flag
 
 if [ "$(tr '[:upper:]' '[:lower:]' <<<"${flag:0:1}")" = "n" ]; then
   echo "exit"
   exit
 fi
 
-for depend in "${DOCKER_DEPEND_LIST[@]}" ; do
-    yum install -y "${depend}"
+for depend in "${DOCKER_DEPEND_LIST[@]}"; do
+  yum install -y "${depend}"
 done
 
-yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo \
-  && yum makecache fast
+yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo &&
+  yum makecache fast
 
 yum install -y docker-ce
 
-echo "${SOFTWARE_NAME}安装完成，设置镜像源"
+echo "${DOCKER_NAME}安装完成，设置镜像源"
 
 CONFIG_FILE=/etc/docker/daemon.json
 
@@ -31,4 +31,23 @@ fi
 echo '{"registry-mirrors": ["https://83tnzmjf.mirror.aliyuncs.com"]}' >>"${CONFIG_FILE}"
 systemctl daemon-reload && systemctl restart docker && systemctl enable docker
 
-echo "设置完毕"
+echo "Docker设置完毕"
+
+DOCKER_COMPOSE_VERSION=1.26.0
+DOCKER_COMPOSE_PATH=/usr/local/bin/docker-compose
+
+read -rep "是否需要安装Docker-Compose? (y/n): " flag
+
+if [ "$(tr '[:upper:]' '[:lower:]' <<<"${flag:0:1}")" = "n" ]; then
+  echo "exit"
+  exit
+fi
+
+read -rep "请输入Docker-Compose的版本号(默认为${DOCKER_COMPOSE_VERSION})" version
+
+if [ -n "${version}" ]; then
+  DOCKER_COMPOSE_VERSION=version
+fi
+
+curl -L "https://get.daocloud.io/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o ${DOCKER_COMPOSE_PATH} \
+  && chmod +x ${DOCKER_COMPOSE_PATH}
